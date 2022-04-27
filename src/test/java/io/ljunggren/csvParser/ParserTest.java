9241d54098452c3a9cd6c0071e2ac745e91a69e6
Alex Ljunggren <aljunggren@dminc.com>
2022-04-27 08:16:03 -0400

    More tests

--- src/test/java/io/ljunggren/csvParser/ParserTest.java
+++ src/test/java/io/ljunggren/csvParser/ParserTest.java
@@ -36,7 +36,6 @@ public class ParserTest {
     public void parseWithHeaderMapping() throws Exception {
         File file = getTestFile("io/ljunggren/csvParser/testWithHeadersOutOfOrder.csv");
         Map<String, String> columnMap = new HashMap<>();
-        // target (object), CSV
         columnMap.put("A", "E");
         columnMap.put("B", "D");
         columnMap.put("D", "B");
@@ -44,6 +43,19 @@ public class ParserTest {
         List<TestPojo> results = parser.parse(file, TestPojo.class);
         assertEquals(3, results.size());
 
+    }
+
+    @Test
+    public void parseWithHeaderMappingWithMissingColumn() throws Exception {
+        File file = getTestFile("io/ljunggren/csvParser/testWithHeadersOutOfOrder.csv");
+        Map<String, String> columnMap = new HashMap<>();
+        columnMap.put("A", "E");
+        Parser parser = new Parser().firstRowIsHeader().columnMap(columnMap);
+        List<TestPojo> results = parser.parse(file, TestPojo.class);
+        assertEquals(3, results.size());
+        assertEquals(Long.valueOf(1), results.get(0).getId());
+        assertEquals("Alex", results.get(0).getState());
+        assertEquals("Indiana", results.get(0).getName());
     }
 
 }
